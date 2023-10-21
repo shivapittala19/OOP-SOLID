5eaab50bbaedbe24e34b618f11155dcbe5c05aee
shivapittala19 <shivapittala2001@gmail.com>
2023-10-21 09:11:05 +0530

    comments

--- SOLID/Caclucator.py
+++ SOLID/Caclucator.py
@@ -1,7 +1,9 @@
 class Calculator:
     def __init__(self, data_extractor, data_analyzer):
-        self.data_extractor = data_extractor
         self.data_analyzer = data_analyzer
+        #initialising the extractor as per problem statement but have no major use
+        # as we did the required calculations in Analyzer module                   
+        self.data_extractor = data_extractor
 
     def calculate_min_difference(self, column_name1, column_name2,column_name3):
         # data = self.data_extractor.extract_data([column_name1, column_name2])
